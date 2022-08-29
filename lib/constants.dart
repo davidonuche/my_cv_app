5d4a210ae3f5b9bd93f191c418cac99a736c8874
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2022-08-29 12:48:07 +0100

    constants

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+const TextStyle kRegularTextStyle =
+    TextStyle(fontFamily: "Bebas", fontSize: 25);
+
+const TextStyle kTitleTextStyle =
+    TextStyle(fontFamily: "Bebas", fontSize: 40, fontWeight: FontWeight.bold);
+    
