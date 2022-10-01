ceb9538fef2ff8a8558501988e44e3e04a5e0160
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2022-10-01 03:55:59 +0100

    unused import

--- lib/screens/my_cv_page.dart
+++ lib/screens/my_cv_page.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import '../constants.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class MyCVPage extends StatelessWidget {
   const MyCVPage({Key? key}) : super(key: key);
