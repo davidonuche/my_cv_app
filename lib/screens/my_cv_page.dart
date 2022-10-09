a29675873a3a0836a1ca41490473bea1cc62a9ff
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2022-10-09 06:51:28 +0100

    unsed import

--- lib/screens/my_cv_page.dart
+++ lib/screens/my_cv_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:url_launcher/url_launcher.dart';
 import '../constants.dart';
 
 class MyCVPage extends StatelessWidget {
@@ -10,69 +11,70 @@ class MyCVPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
-          children: [
-            //Avatar Image
-            SizedBox(height: 20),
-            CircleAvatar(
-              radius: 90,
-              foregroundImage: AssetImage("assets/picture.jpeg"),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "David Onuche",
-              style: kTitleTextStyle,
-            ),
-            Text(
-              "Software Engineer",
-              style: kRegularTextStyle,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Card(
-                margin: EdgeInsets.all(8),
-                child: ListTile(
-                  leading: Icon(Icons.call, color: Colors.blue, size: 30),
-                  title: Center(
-                    child: 
-                    Text("+234 813 917 5123", style: kCardElementTextStyle)),
-                    trailing: SizedBox(),
-                  ),
-                ),
+            children: [
+              //Avatar Image
+              SizedBox(height: 20),
+              CircleAvatar(
+                radius: 90,
+                foregroundImage: AssetImage("assets/picture.jpeg"),
               ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Card(
-                child: ListTile(
-                    leading:
-                        Icon(Icons.email, color: Colors.green, size: 30),
-                    title: Center(
-                      child: Text(
-                        "onuchedavid34@gmail.com",
-                        style: kCardElementTextStyle,
+              SizedBox(height: 10),
+              Text(
+                "David Onuche",
+                style: kTitleTextStyle,
+              ),
+              Text(
+                "Software Engineer",
+                style: kRegularTextStyle,
+              ),
+              Padding(
+               padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(Icons.call, color: Colors.blue, size: 30),
+                      title: Center(
+                        child: Text(
+                          "+234 813 917 5123",
+                          style: kCardElementTextStyle,
+                        ),
                       ),
+                      trailing: SizedBox(),
                     ),
-                    trailing: SizedBox(),
                   ),
                 ),
-              ),
-                    SizedBox(height: 15),
-                    Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Card(
-                child: ListTile(
-                    leading:
-                    Icon(Icons.hub, color: Colors.red, size: 30),
-                    title: Center(
-                      child: Text(
-                        "github: Davidbeebest",
-                        style: kCardElementTextStyle,
+                SizedBox(height: 3),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(Icons.email, color: Colors.green, size: 30),
+                      title: Center(
+                        child: Text(
+                          "onuchedavid34@gmail.com",
+                          style: kCardElementTextStyle,
+                        ),
                       ),
+                      trailing: SizedBox(),
                     ),
-                    trailing: SizedBox(),
                   ),
                 ),
-              ),
-                    SizedBox(height: 15),
+              SizedBox(height: 3),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(Icons.hub, color: Colors.red, size: 30),
+                      title: Center(
+                        child: Text(
+                          "Github: Davidbeebest",
+                          style: kCardElementTextStyle,
+                        ),
+                      ),
+                      trailing: SizedBox(),
+                    ),
+                  ),
+                ),
+              SizedBox(height: 3),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
@@ -83,7 +85,8 @@ class MyCVPage extends StatelessWidget {
                       child: Column(
                         children: [
                           Text("Education",
-                              textAlign: TextAlign.center, style: kBoldTextStyle),
+                              textAlign: TextAlign.center,
+                              style: kBoldTextStyle),
                           SizedBox(height: 3),
                           Text("Student",
                               textAlign: TextAlign.center,
@@ -94,25 +97,26 @@ class MyCVPage extends StatelessWidget {
                           Text("Political Science",
                               textAlign: TextAlign.center,
                               style: kRegularSmallTextStyle),
-                              Text("African Leadership X",
+                          Text("African Leadership X",
                               textAlign: TextAlign.center,
                               style: kRegularSmallTextStyle),
-                              Text("Software Engineering",
+                          Text("Software Engineering",
                               textAlign: TextAlign.center,
                               style: kRegularSmallTextStyle),
                         ],
                       ),
                     ),
-                        Expanded(
+                    Expanded(
                       child: Column(
                         children: [
                           Text("Work Experience",
-                              textAlign: TextAlign.center, style: kBoldTextStyle),
+                              textAlign: TextAlign.center,
+                              style: kBoldTextStyle),
                           SizedBox(height: 3),
                           Text("Flutter Developer",
                               textAlign: TextAlign.center,
                               style: kRegularSmallTextStyle),
-                              Text("Asst Flutter Lead @GDSC OUI",
+                          Text("Asst Flutter Lead @GDSC OUI",
                               textAlign: TextAlign.center,
                               style: kRegularSmallTextStyle),
                           Text("Dart, Shell, C, Py, JS",
